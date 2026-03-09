c622b06d6bec1538988262737d90a70a1d971421
lycurtis <lycurtis046@gmail.com>
2026-03-09 01:16:01 -0400

    working!

--- app/main.c
+++ app/main.c
@@ -26,6 +26,41 @@ int main(void) {
 
     printf("LiDAR interface\r\n");
 
+    static sf_parse_ctx_t lidar_ctx;
+    sf_parser_init(&lidar_ctx);
+
+    uint8_t tx_buf[16];
+    uint16_t len = sf_build_read_request(0, tx_buf, sizeof(tx_buf));
+
+    printf("TX packet (%u bytes): ", len);
+    for (uint16_t i = 0; i < len; i++) {
+        printf("%02X ", tx_buf[i]);
+    }
+    printf("\r\n");
+
+    BSP_Delay_ms(1000);
+    usart_write(BSP_USART_LIDAR, tx_buf, len);
+    BSP_Delay_ms(100);
+    usart_write(BSP_USART_LIDAR, tx_buf, len);
+
+    uint32_t rx_count = 0;
+
     for (;;) {
+        while (usart_rx_ready(BSP_USART_LIDAR)) {
+            uint8_t b = usart_read_byte(BSP_USART_LIDAR);
+            printf("%02X ", b);
+            rx_count++;
+            if (sf_parser_feed(&lidar_ctx, b)) {
+                if (lidar_ctx.last_packet.cmd_id == 0) {
+                    printf("\r\nProduct: %.*s\r\n",
+                           lidar_ctx.last_packet.payload_len,
+                           lidar_ctx.last_packet.payload);
+                }
+            }
+        }
+
+        printf("\r\n[RX=%lu] Sending request...\r\n", rx_count);
+        usart_write(BSP_USART_LIDAR, tx_buf, len);
+        BSP_Delay_ms(500);
     }
 }
